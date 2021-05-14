e121242758cb87c8d2396e23e62e01709d7822df
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-13 23:18:58 -0400

    add controller for route '/product'

--- app.js
+++ app.js
@@ -20,17 +20,22 @@ app.use((req, res, next) => {
 */
 
 app.use('/', (req, res, next) => {
-  console.log('This always runs!');
+  // console.log('This always runs!');
   next();
 })
 
 app.use('/add-product', (req, res, next) => {
-  console.log('Im still in middleware!');
-  res.send('<form><input type="text" name="title"><button type="submit">Add Product</button></form>')
+  // console.log('Im still in middleware!');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
-  console.log('I should not be running run!');
+  // console.log('I should not be running run!');
   res.send('<h1>Hello from Express.js!</h1>')// auto sets 'Content-Type: text/html; charset=utf-8' in headers
 })
 

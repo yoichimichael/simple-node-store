67a8b0f951967a9b4455bdf57951500ee2439f07
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-15 17:25:49 -0400

    add error handler routing

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.use(express.urlencoded({extended: false}));
 // first middleware with route specifi
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+});
+
+// CREATE AND START SERVER
 /*
 // no longer need becuase of Express app methods
 const server = http.createServer(app);

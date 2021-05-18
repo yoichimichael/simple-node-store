3b521172648a60bf48488a53b9d55243c3e533f8
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-17 23:59:21 -0400

    included html to shop.hjs:

--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // console.log('I should not be running run!');
-  res.send('<h1>Hello from Express.js!</h1>')// auto sets 'Content-Type: text/html; charset=utf-8' in headers
+  // res.send('<h1>Hello from Express.js!</h1>')// auto sets 'Content-Type: text/html; charset=utf-8' in headers
+  res.sendFile('../views/shop.html');
 });
 
 module.exports = router;

ded1086a33dfcae06c0cd45de834263013004718
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-19 13:01:11 -0400

    add basic styling to shop view

--- routes/admin.js
+++ routes/admin.js
@@ -11,9 +11,9 @@ router.get('/add-product', (req, res, next) => {
   // res.send(
   //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   // );
-
+  
   // used __dirname before instead of importing rootDir
-  res.sendFile(path.join(rootDir, '..', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 // '/admin/add-product' => POST
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   // res.send('<h1>Hello from Express.js!</h1>')// auto sets 'Content-Type: text/html; charset=utf-8' in headers
-  res.sendFile(path.join(rootDir, '../', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;

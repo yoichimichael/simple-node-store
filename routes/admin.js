cd4a414eabb2f53c0461826f55cc981111bb02cf
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-21 01:41:50 -0400

    clean up code

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ router.get('/add-product', (req, res, next) => {
   //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   // );
   
-  // used __dirname before instead of importing rootDir
+  // used __dirname previously instead of importing rootDir
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 

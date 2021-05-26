b838e1e9fbfc4945a7ba8a6954a5a05b8ff11227
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-26 18:45:11 -0400

    clean up shop.pug file

--- routes/shop.js
+++ routes/shop.js
@@ -8,10 +8,14 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
+// router.get('/', (req, res, next) => {
+//   console.log('shop.js: ',adminData.products);
+//   // res.send('<h1>Hello from Express.js!</h1>')// auto sets 'Content-Type: text/html; charset=utf-8' in headers
+//   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+// });
+
 router.get('/', (req, res, next) => {
-  console.log('shop.js: ',adminData.products);
-  // res.send('<h1>Hello from Express.js!</h1>')// auto sets 'Content-Type: text/html; charset=utf-8' in headers
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;

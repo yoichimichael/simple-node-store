7df2d264edacc369b6dd64c492b2cbb0235fc383
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-27 15:09:16 -0400

    add data object to render method for shop view in shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -8,14 +8,17 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
+// SENDING A STATIC FILE
 // router.get('/', (req, res, next) => {
 //   console.log('shop.js: ',adminData.products);
 //   // res.send('<h1>Hello from Express.js!</h1>')// auto sets 'Content-Type: text/html; charset=utf-8' in headers
 //   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 // });
 
+// RENDERING A TEMPLATE
 router.get('/', (req, res, next) => {
-  res.render('shop');
+  const products = adminData.products;
+  res.render('shop', { docTitle: 'Shop', prods: products});
 });
 
 module.exports = router;

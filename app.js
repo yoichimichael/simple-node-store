f045382ce716c066aae1bf23a48ea0f5a88cece2
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-28 22:26:57 -0400

    add 404 pug file and change routing to render pug file

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ app.use(shopRoutes);
 // ERROR HANDLER
 app.use((req, res, next) => {
   // res.status(404).send('<h1>Page not found</h1>')
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { docTitle: 'Page Not Found'});
 });
 
 // CREATE AND START SERVER

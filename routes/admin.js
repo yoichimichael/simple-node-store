6c3766290bec2891e031d2b88607447fe8f98f81
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-28 22:20:19 -0400

    modify shop route to render pug template

--- routes/admin.js
+++ routes/admin.js
@@ -9,13 +9,22 @@ const router = express.Router();
 const products = [];
 
 // '/admin/add-product'=> GET
+// router.get('/add-product', (req, res, next) => {
+//   // res.send(
+//   //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+//   // );
+  
+//   // used __dirname previously instead of importing rootDir
+//   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+// })
+
 router.get('/add-product', (req, res, next) => {
   // res.send(
   //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   // );
   
   // used __dirname previously instead of importing rootDir
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { docTitle: 'Add Product' });
 })
 
 // '/admin/add-product' => POST

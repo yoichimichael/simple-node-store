1319a7b389497186c8c20594d8d7c7f740c288fb
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-06-08 00:51:18 -0400

    refactor shop header partial

--- routes/shop.js
+++ routes/shop.js
@@ -20,7 +20,8 @@ router.get('/', (req, res, next) => {
   const products = adminData.products;
   res.render('shop', { 
     pageTitle: 'Shop', 
-    prods: products, path: '/', 
+    prods: products, 
+    path: '/', 
     hasProducts: products.length > 0,
     activeShop: true,
     productCSS: true

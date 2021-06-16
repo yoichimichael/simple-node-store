0277e9424c7cdda1c8e195b9476e9b588ae9cdcc
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-06-15 23:59:16 -0400

    add product fuction to admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../helpers/path');
+// const rootDir = require('../helpers/path');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
@@ -18,21 +20,7 @@ const products = [];
 //   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 // })
 
-router.get('/add-product', (req, res, next) => {
-  // res.send(
-  //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  // );
-  
-  // used __dirname previously instead of importing rootDir
-  
-  res.render('add-product', { 
-    pageTitle: 'Add Product', 
-    path: '/admin/add-product',
-    productCSS: true,
-    formsCSS: true,
-    activeAddProduct: true 
-  });
-})
+router.get('/add-product', productsController.getAddProduct);
 
 // '/admin/add-product' => POST
 router.post('/add-product', (req, res, next) => {

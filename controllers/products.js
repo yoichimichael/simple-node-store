e40c30b3e66beca0d0019c2d7718e7e7b921c618
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-06-15 23:57:28 -0400

    add products.js controller file, add first routing function

---
+++ controllers/products.js
@@ -0,0 +1,15 @@
+exports.getAddProduct = (req, res, next) => {
+  // res.send(
+  //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  // );
+  
+  // used __dirname previously instead of importing rootDir
+  
+  res.render('add-product', { 
+    pageTitle: 'Add Product', 
+    path: '/admin/add-product',
+    productCSS: true,
+    formsCSS: true,
+    activeAddProduct: true 
+  });
+}

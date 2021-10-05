9e2948f0c07c73dcf7dd1de31f3830ee64348935
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-04 23:53:37 -0700

    add individual product route

--- routes/shop.js
+++ routes/shop.js
@@ -20,6 +20,8 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId');
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);

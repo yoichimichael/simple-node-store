a4e21a36ad875f3af2ccd5df19ad1f73971658e6
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-09 19:59:35 -0700

    add Cart class and static addProduct method

--- models/cart.js
+++ models/cart.js
@@ -1,3 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../helpers/path');
+const p = path.join(
+  rootDir, 
+  'data', 
+  'cart.json'
+);
+
 module.exports = class Cart {
+
+  static addProduct(id, productPrice){
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      const existingProduct = cart.products.find(prod => prod.id === id);
+      let updatedProduct;
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+      } else {
+        updatedProduct = { id: id, qty: 1};
+      }
+      cart.totalPrice = cart.totalPrice + productPrice;
+    }) 
+  }
   
 }

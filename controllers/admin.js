dd0cba5cc5cf6589c649aef28e1e4cf7ac6f941e
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-11 19:41:18 -0700

    pass product data to Edit Product view

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,11 +21,19 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect('/');
   }
-  res.render('admin/edit-product', { 
-    pageTitle: 'Edit Product', 
-    path: '/admin/edit-product',
-    editing: editMode
-  });
+
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', { 
+      pageTitle: 'Edit Product', 
+      path: '/admin/edit-product',
+      editing: editMode,
+      product
+    });
+  })
 }
 
 exports.getProducts = (req, res, next) => {

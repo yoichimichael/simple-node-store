6e9de2b94a5d8836950b2c9e499b8e545cf3e23a
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-11 19:47:05 -0700

    fix add product display bug

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', { 
     pageTitle: 'Add Product', 
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    editing: false
   });
 }
 

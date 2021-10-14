d865c188f9fc07deb6ebb8637bffb71bab7d887a
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-13 23:57:04 -0700

    refactor product model save function

--- models/product.js
+++ models/product.js
@@ -14,9 +14,9 @@ const getProductsFromFile = (cb) => {
   });
 }
 
-
 module.exports = class Product {
-  constructor({title, imageUrl, description, price}){
+  constructor({id, title, imageUrl, description, price}){
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -24,8 +24,14 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+    
     getProductsFromFile(products => {
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => {
+          prod.id === this.id;
+        });
+      }
+      this.id = Math.random().toString();
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err); 

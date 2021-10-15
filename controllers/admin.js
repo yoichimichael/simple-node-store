b7bf042b4fb71939829b2cb1d97a2ce55df1b378
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-14 23:35:53 -0700

    add delete product route and controller function

--- controllers/admin.js
+++ controllers/admin.js
@@ -58,3 +58,7 @@ exports.getProducts = (req, res, next) => {
     });
   });
 }
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+}
--- routes/admin.js
+++ routes/admin.js
@@ -30,4 +30,6 @@ router.get('/edit-product/:productId', adminController.getEditProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
 
+router.post('/delete-product', adminController.postDeleteProduct);
+
 module.exports = router;

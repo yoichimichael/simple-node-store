18405c2508a2b2cb39f4f27de3fc93423e525ec2
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-18 00:00:58 -0700

    finish delete product static method

--- models/cart.js
+++ models/cart.js
@@ -42,9 +42,13 @@ module.exports = class Cart {
       const updatedCart = { ...cart };
       const product = updatedCart.products.find(prod => prod.id === id);
       const productQty = product.qty;
-      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
 
+      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
       updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+        console.log(err);
+      })
     });
   }
 

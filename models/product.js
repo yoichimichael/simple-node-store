44a6ba8dfaa53c65b8a4d1a57d7581b2b3b2e222
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-18 00:03:32 -0700

    update deleteById static method on Product

--- models/product.js
+++ models/product.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 // const { getProducts } = require('../controllers/products');
 
+const Cart = require('./cart');
+
 const rootDir = require('../helpers/path');
 
 const p = path.join(rootDir, 'data', 'products.json');
@@ -47,8 +49,13 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
       const updatedProducts = products.filter(p => p.id !== id);
-
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      })
       cb(product);
     })
   }

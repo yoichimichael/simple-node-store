17ee0d25886e759f8baad96a4f30d1c4bc143c58
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-19 23:37:04 -0700

    add delete product route

--- routes/shop.js
+++ routes/shop.js
@@ -26,8 +26,12 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
+router.post('/cart-delete-item');
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
+
+
 
 module.exports = router;

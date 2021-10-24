86bb39ba6a1623da7b96d38bc4875e4cdc7b8c0f
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-24 12:27:03 -0700

    add env vars for db hookup

--- helpers/database.js
+++ helpers/database.js
@@ -1,12 +1,14 @@
 const { Pool, Client } = require('pg')
 
-const pool = new Pool()
+const pool = new Pool({
+  user: 'root',
+  host: 'localHost'
+})
 
 pool.query('SELECT NOW()', (err, res) => {
   console.log(err, res)
   pool.end()
 })
-
 
 const res = await pool.query('SELECT NOW()')
 await pool.end()

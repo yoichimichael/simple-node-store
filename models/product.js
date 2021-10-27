7d892b29690e30b51f9e10cfa1e1d10b29f4a935
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-27 01:14:31 -0700

    debug insert query syntax

--- models/product.js
+++ models/product.js
@@ -6,13 +6,13 @@ module.exports = class Product {
   constructor({id, title, imageUrl, description, price}){
     this.id = id;
     this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
     this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
   }
 
   save() {
-    const sqlQuery = 'INSERT INTO products (title, price, description, imageUrl) VALUES ($1, $2, $3, $4)';
+    const sqlQuery = 'INSERT INTO products(id, title, price, description, "imageUrl") VALUES(DEFAULT, $1, $2, $3, $4)';
     const values = [this.title, this.price, this.description, this.imageUrl];
     return db_pool.query(sqlQuery, values);
   }

ea745f2b10e2260f44c040ac4709b23208fefe3f
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-28 23:56:41 -0700

    debug product view load

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,8 +14,9 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId).then(({ rows }) => {
+    const product = rows[0];
     res.render('shop/product-detail', { 
-      product: rows[0],
+      product,
       pageTitle: product.title, 
       path: '/products' 
     })

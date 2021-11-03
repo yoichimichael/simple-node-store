1f8a449f244724e7fc4bc6fc277e8e49659261f3
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-11-02 23:46:09 -0700

    refactor db code to connect via sequelize

--- helpers/database.js
+++ helpers/database.js
@@ -1,6 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.PG_DB_CONN);
+
+module.exports = sequelize;
+
+/* 
+=== Connecting to DB with pg package ===
+
 const { Pool } = require('pg')
 const connectionString = process.env.PG_DB_CONN;
 
 module.exports = new Pool({
   connectionString
 });
+*/
+
+

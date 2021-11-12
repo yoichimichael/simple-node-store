cf8de360a9dbbafe9201ca7859c91b1532cad17b
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-11-11 23:56:21 -0800

    refactor get cart

--- app.js
+++ app.js
@@ -73,8 +73,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1);
   })
@@ -85,7 +85,10 @@ sequelize
     return user;
   })
   .then(user => {
-    console.log(user);
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
   .catch(console.log);
--- controllers/shop.js
+++ controllers/shop.js
@@ -33,22 +33,27 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (let product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id);
-        if (cartProductData) {
-          cartProducts.push({productData: product, qty: cartProductData.qty});
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
-    });
-  })
+  req.user.getCart()
+    .then(cart => {
+      console.log(cart)
+    })
+    .catch(console.log);
+  // Cart.getCart(cart => {
+  //   Product.fetchAll(products => {
+  //     const cartProducts = [];
+  //     for (let product of products) {
+  //       const cartProductData = cart.products.find(prod => prod.id === product.id);
+  //       if (cartProductData) {
+  //         cartProducts.push({productData: product, qty: cartProductData.qty});
+  //       }
+  //     }
+  //     res.render('shop/cart', {
+  //       path: '/cart',
+  //       pageTitle: 'Your Cart',
+  //       products: cartProducts
+  //     });
+  //   });
+  // })
 }
 
 exports.postCart = (req, res, next) => {

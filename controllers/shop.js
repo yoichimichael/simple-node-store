777ae18baff29a7ca07593324161e0ee9de90a5e
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-11-16 00:08:08 -0800

    finish order flow

--- controllers/shop.js
+++ controllers/shop.js
@@ -126,7 +126,8 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders()
+  req.user
+    .getOrders({ include: ['products'] })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',

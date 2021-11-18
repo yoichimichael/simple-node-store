466a21ad308e74297f01d1cdd0683f6255a9692d
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-11-17 22:37:10 -0800

    clean up app.js

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-// CORE MODULES
-// const http = require('http'); // not needed because Express calls on the module itself
+require('dotenv').config();
 const path = require('path');
 
-// 3RD PARTY PACKAGES
-
-// variable 'express' is a top level function
 const express = require('express');
-const sequelize = require('./helpers/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
-require('dotenv').config();
+
+const errorsController = require('./controllers/errors');
 
 // DEPRECATED - urlencoded() and static() are now part of the express object
 // const bodyParser = require('body-parser');
@@ -30,20 +20,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const errorsController = require('./controllers/errors');
 
-/*
-// example use of next()
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); // allows request to continue to next middleware
-})
-*/
-
-// app.use('/', (req, res, next) => {
-//   // console.log('This always runs!');
-//   next();
-// })
 
 // parses ALL incoming request bodies
 // automatically calls next()
@@ -66,44 +43,3 @@ app.use(shopRoutes);
 
 // ERROR HANDLER
 app.use(errorsController.getPageNotFound);
-
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product); // redundant, but makes relationship clear
-User.hasOne(Cart);
-Cart.belongsTo(User); // also redundant, but makes relationship clear
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: 'Yoichi', email: 'test@testing.com' });
-    } 
-    return user;
-  })
-  .then(user => {
-    // console.log(user);
-    return user.createCart();
-  })
-  .then(cart => {
-    app.listen(3000);
-  })
-  .catch(console.log);
-
-// CREATE AND START SERVER
-// no longer need becuase of Express app methods
-/*
-const server = http.createServer(app);
-server.listen(3000);
-*/
-
-// instead of the above:
-// app.listen(3000);

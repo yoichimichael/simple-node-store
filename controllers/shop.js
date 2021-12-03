974b89611adae1ba13e6135150fd7cc3a735ed33
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-12-02 23:34:18 -0800

    update postOrder method

--- controllers/shop.js
+++ controllers/shop.js
@@ -74,29 +74,9 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   let fetchedCart;
   req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then(products => {
-      return req.user
-        .createOrder()
-        .then(order => {
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .then(result => {
-          return fetchedCart.setProducts(null);
-        })
-        .then(results => {
-          res.redirect('/orders');
-        })
-        .catch(console.log)
+    .addOrder()
+    .then(results => {
+      res.redirect('/orders');
     })
     .catch(console.log)
 }

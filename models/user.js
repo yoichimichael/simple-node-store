b8e60d96171247d2046806503e88bfa09a87642e
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-12-02 23:48:09 -0800

    update addOrder to add more product info to order products

--- models/user.js
+++ models/user.js
@@ -77,9 +77,19 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name
+          }
+        };
+        return db
+          .collection('orders')
+          .insertOne(order)
+          })
       .then(result => {
         this.cart = {items: []};
         return db
@@ -89,6 +99,12 @@ class User {
             { $set: { cart: { items: [] } } }
           );
       }) 
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
   }
 
   static findById(userId){

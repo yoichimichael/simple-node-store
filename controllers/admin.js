c7e899f9401679c633678dc99c7a18d56a9c3c32
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-12-08 23:48:42 -0800

    refactor post delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -77,7 +77,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(result => {
       res.redirect('/admin/products');
     })

4dd4d7619192b455bdcce533200b43be19400c96
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-12-12 22:59:53 -0800

    refactor post to cart flow

--- controllers/admin.js
+++ controllers/admin.js
@@ -67,6 +67,7 @@ exports.postEditProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
+      console.log(products);
       res.render('admin/products', { 
         prods: products, 
         pageTitle: 'Admin Products',

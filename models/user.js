6fc0dbb42be6a9ce4c41703a3daddbb9275e8a3e
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-12-14 23:30:37 -0500

    reafctor delete from cart flow

--- models/user.js
+++ models/user.js
@@ -51,8 +51,12 @@ userSchema.methods.addToCart = function(product) {
     return this.save()
 };
 
-userSchema.methods.getCart = function() {
-
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  })
+  this.cart.items = updatedCartItems;
+  return this.save();
 }
 
 module.exports = mongoose.model('User', userSchema);

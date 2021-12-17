7a2c8a0c0a5a17e296231346d4b2bdbb95b58329
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-12-16 22:40:29 -0500

    add clearCart method to User schema

--- models/user.js
+++ models/user.js
@@ -58,6 +58,10 @@ userSchema.methods.removeFromCart = function(productId) {
   this.cart.items = updatedCartItems;
   return this.save();
 }
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
+  return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
 

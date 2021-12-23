a9dc24985782cfe8997612029f956adbef98cccf
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-12-23 13:50:14 -0500

    set logged in cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get('Cookie').split('=')[1];
+  console.log(isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login Orders',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 }
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie');
+  res.set('Set-Cookie', 'loggedIn=true');
   res.redirect('/');
 }

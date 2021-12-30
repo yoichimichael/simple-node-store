ed02c66430b9ec863bdb49c0ca292eb4e8c40089
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-12-29 22:57:02 -0800

    add user email to session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get('Cookie').split('=')[1];
   // console.log(isLoggedIn);
@@ -10,7 +12,16 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  let user;
+  User.find({ email: email })
+    .then(user => {
+      user = user;
+    })
+    .catch(console.log);
   // isLoggedIn property is custom; a property of any name can be added to the session object
+  if (user)
+    req.session.user = user;
   req.session.isLoggedIn = true;
   res.redirect('/');
 }

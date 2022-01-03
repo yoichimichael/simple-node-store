748b63e39fd32115b6def14be6804ee7e95c304d
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-01-02 23:49:09 -0800

    add signup view controller actions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,17 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get('Cookie').split('=')[1];
-  // console.log(isLoggedIn);
-  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login Orders',
+    isAuthenticated: false
+  });
+}
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
     isAuthenticated: false
   });
 }
@@ -26,6 +31,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch(console.log);
 }
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {

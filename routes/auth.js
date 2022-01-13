517b2bf2df9c6281786450c47f0146dacaac8abc
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-01-12 23:47:05 -0800

    add reset route and link on login view

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,6 @@ router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
+router.get('/reset-password', authController.getReset);
+
 module.exports = router;

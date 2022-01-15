1670ff37c37cd47d4a3e781ed92ee01280b9a229
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-01-15 14:26:40 -0800

    add new password view

--- controllers/auth.js
+++ controllers/auth.js
@@ -138,7 +138,7 @@ exports.postReset = (req, res, next) => {
             from: 'ynagano@pm.me',
             subject: 'Password reset',
             html: `
-              <p>You requested a password reset</p>
+              <p>You requested a password reset.</p>
               <p>Click this <a href="http://localhost:3000/reset-password/${token}">link</a> to set a new password.</p>
             `
           })

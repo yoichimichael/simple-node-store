bc9f544f7d63f628a9fa698ddcfeb62367426bc5
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-01-20 19:59:27 -0800

    add conditional class value for signup inputs

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,7 +28,8 @@ exports.getSignup = (req, res, next) => {
       email: '',
       password: '',
       confirmPassword: ''
-    }
+    },
+    validationErrors: []
   });
 }
 
@@ -76,6 +77,7 @@ exports.postSignup = (req, res, next) => {
   const { email, password } = req.body;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    console.log(errors.array());
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
@@ -84,7 +86,8 @@ exports.postSignup = (req, res, next) => {
         email,
         password,
         confirmPassword: req.body.confirmPassword
-      }
+      },
+      validationErrors: errors.array()
     });
   }
 

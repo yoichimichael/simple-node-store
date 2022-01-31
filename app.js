656f28e4d916437d830605ba891bb1bdff0c28b1
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-01-31 14:27:12 -0800

    add error throwing comment

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
+      // errors within async must be passed to next() to be passed to error handling middleware
       next(new Error(err));
     });
 })

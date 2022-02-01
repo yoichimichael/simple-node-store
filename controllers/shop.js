eec51fcf7bdc3370d4d4632513a68f3f59da2fa9
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-02-01 13:29:11 -0800

    add getInvoice file retrieval flow

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -151,4 +154,16 @@ exports.getOrders = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+}
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = 'invoice-' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.send(data);
+  })
 }

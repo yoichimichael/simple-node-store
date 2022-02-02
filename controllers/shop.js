61a4ada0fc943706d6e64a1f94e0a3361b96ba5d
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-02-01 21:58:41 -0800

    add use pdfkit to generate basic pdf on getINvoice calls

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -168,6 +170,20 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName);
+
+      const pdfDoc = new PDFDocument();
+      res.setHeader('Content-Type', 'application/pdf'); 
+      res.setHeader(
+        'Content-Disposition', 
+        `inline; filename="${invoiceName}"`
+      ); 
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.text('Hello World!');
+
+      pdfDoc.end();
+
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
       //     return next(err);
@@ -179,13 +195,13 @@ exports.getInvoice = (req, res, next) => {
       //   ); 
       //   res.send(data);
       // });
-      const file = fs.createReadStream(invoicePath);
-      res.setHeader('Content-Type', 'application/pdf'); 
-      res.setHeader(
-        'Content-Disposition', 
-        `inline; filename="${invoiceName}"`
-      ); 
-      file.pipe(res);
+      // const file = fs.createReadStream(invoicePath);
+      // res.setHeader('Content-Type', 'application/pdf'); 
+      // res.setHeader(
+      //   'Content-Disposition', 
+      //   `inline; filename="${invoiceName}"`
+      // ); 
+      // file.pipe(res);
     })
     .catch(next);
 }

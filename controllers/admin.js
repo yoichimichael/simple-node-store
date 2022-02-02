3a2690d873fa491cc1b41a6e2329ef7552961eb2
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-02-02 02:13:15 -0800

    add delete file flow for product images

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,9 @@
-const Product = require('../models/product');
 const { validationResult } = require('express-validator');
+
+const fileHelper = require('../helpers/file');
+const Product = require('../models/product');
+
+
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', { 
@@ -131,6 +135,7 @@ exports.postEditProduct = (req, res, next) => {
       product.title = title;
       product.price = price;
       if (image) {
+        fileHelper.deleteFile(product.imageUrl);
         product.imageUrl = image.path;
       }
       product.description = description;
@@ -165,9 +170,16 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product
-    .deleteOne({ _id: prodId, userId: req.user._id })
+  Product.findById(prodId)
+    .then(product => {
+      if (!product) {
+        return next(new Error('Product not found.'))
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
     .then(result => {
+      console.log('Product Destroyed!')
       res.redirect('/admin/products');
     })
     .catch(err => {

87c02ef47b7c3bdb1e62a2a764cd3f522fa1c7c6
yoichimichael <yoichi.michael.nagano@gmail.com>
2022-02-02 21:43:30 -0800

    starting to build out async delete request

--- controllers/admin.js
+++ controllers/admin.js
@@ -166,8 +166,8 @@ exports.getProducts = (req, res, next) => {
     });
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -178,11 +178,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(result => {
       console.log('Product Destroyed!')
-      res.redirect('/admin/products');
+      res.status(200).json({ message: 'Success!' });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: 'Deleting product failed.' });
     });
 }
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,3 @@
-const deleteProduct = (id, token) => {
-  // console.log(btn.parentNode.querySelector('[name=productId]'))
-  console.log(id, token);
-  // console.log(id);
+const deleteProduct = (prodId, csrfToken) => {
+  fetch(`/product/${prodId}`)
 }
